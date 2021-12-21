2a544905f0674fcf3174a0e97748ceaf70812b55
theander <aclaudino@gmail.com>
2021-12-21 14:42:47 -0300

    MD5 encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const PORT = process.env.PORT || 3000;
 
 
@@ -26,9 +26,7 @@ const userSchema = new mongoose.Schema({
   email: String
 });
 
-const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFiels: ["password"], excludeFromEncryption: ["email"] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -45,7 +43,7 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({ email: req.body.username, password: req.body.password });
+  const newUser = new User({ email: req.body.username, password: md5(req.body.password) });
   console.log(newUser);
   newUser.save((err) => {
     if (err) {
@@ -58,7 +56,7 @@ app.post("/register", (req, res) => {
 });
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
 
